59b7fd971e500543b8c9b354eac79e5ce8f4393f
PIYUJIN <yjyj020201@gmail.com>
2023-07-07 16:51:49 +0900

    [Create] #6  메모 activity 생성

---
+++ app/src/main/java/com/test/category_memo/MemoMainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.category_memo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MemoMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo_main)
+    }
+}
