21606512c5d0bd8ea61eddb028ef2c077ea0cd7d
PIYUJIN <yjyj020201@gmail.com>
2023-07-07 17:05:51 +0900

    [Create] #7  메모 등록 activity 생성

---
+++ app/src/main/java/com/test/category_memo/MemoAddActivity.kt
@@ -0,0 +1,11 @@
+package com.test.category_memo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MemoAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo_add)
+    }
+}
