cecc83865947bb5f7f366b8b31ec0b90fb810ce2
PIYUJIN <yjyj020201@gmail.com>
2023-07-08 19:01:03 +0900

    [Feat] #2  로그인 기능 구현

--- app/src/main/java/com/test/category_memo/LoginActivity.kt
+++ app/src/main/java/com/test/category_memo/LoginActivity.kt
@@ -1,11 +1,40 @@
 package com.test.category_memo
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.test.category_memo.database.PasswordDAO
+import com.test.category_memo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var activityLoginBinding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        activityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(activityLoginBinding.root)
+
+        activityLoginBinding.run {
+            toolbarLogin.run {
+                title = "로그인"
+                setTitleTextColor(Color.WHITE)
+            }
+
+            buttonLogin.setOnClickListener {
+                var passwordClass = PasswordDAO.selectData(this@LoginActivity,1)
+                var password = passwordClass.pw
+
+                if(editTextTextPasswordLogin.text.toString() == password) {
+                    var categoryIntent = Intent(this@LoginActivity,MainActivity::class.java)
+                    startActivity(categoryIntent)
+                }
+                else {
+                    var t1 = Toast.makeText(this@LoginActivity,"비밀번호가 잘못되었습니다.",Toast.LENGTH_SHORT)
+                    t1.show()
+                }
+            }
+        }
     }
 }
