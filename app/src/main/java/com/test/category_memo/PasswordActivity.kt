ebe7c0aa9268166899866c3ab952953864d67f31
PIYUJIN <yjyj020201@gmail.com>
2023-07-08 17:31:44 +0900

    [Design] #1  툴바 타이틀 설정

--- app/src/main/java/com/test/category_memo/PasswordActivity.kt
+++ app/src/main/java/com/test/category_memo/PasswordActivity.kt
@@ -1,11 +1,26 @@
 package com.test.category_memo
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.category_memo.databinding.ActivityPasswordBinding
 
 class PasswordActivity : AppCompatActivity() {
+
+    lateinit var activityPasswordBinding: ActivityPasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_password)
+        activityPasswordBinding = ActivityPasswordBinding.inflate(layoutInflater)
+        setContentView(activityPasswordBinding.root)
+
+        activityPasswordBinding.run {
+            toolbarPassword.run {
+                title = "비밀번호 설정"
+                setTitleTextColor(Color.WHITE)
+            }
+
+
+
+        }
     }
 }
