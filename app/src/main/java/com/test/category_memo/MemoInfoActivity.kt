48985d4ca9ce48e39a3c4e30b9943bc94d5da0b2
PIYUJIN <yjyj020201@gmail.com>
2023-07-09 14:34:43 +0900

    [Feat] #8  메모 내용 보기 기능 생성

--- app/src/main/java/com/test/category_memo/MemoInfoActivity.kt
+++ app/src/main/java/com/test/category_memo/MemoInfoActivity.kt
@@ -1,11 +1,69 @@
 package com.test.category_memo
 
+import android.content.Intent
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.test.category_memo.database.MemoDAO
+import com.test.category_memo.database.MemoInfo
+import com.test.category_memo.databinding.ActivityMemoInfoBinding
 
 class MemoInfoActivity : AppCompatActivity() {
+
+    lateinit var activityMemoInfoBinding: ActivityMemoInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_memo_info)
+        activityMemoInfoBinding = ActivityMemoInfoBinding.inflate(layoutInflater)
+        setContentView(activityMemoInfoBinding.root)
+
+        var position = intent.getIntExtra("position",0)
+        Log.d("lion","position : $position")
+
+        activityMemoInfoBinding.run {
+
+            var memo = MemoDAO.selectData(this@MemoInfoActivity,position)
+            textViewMemoInfoName.text = memo.memoName
+            textViewMemoInfoContent.text = memo.memoContent
+            textViewMemoInfoDate.text = memo.date
+
+            toolbarMemoInfo.run {
+                title = "메모 보기"
+                setTitleTextColor(Color.WHITE)
+
+                inflateMenu(R.menu.info_menu)
+
+                setOnMenuItemClickListener {
+                    when(it.itemId) {
+                        R.id.itemEdit -> {
+                            var memoAddIntent = Intent(this@MemoInfoActivity,MemoEditActivity::class.java)
+                            startActivity(memoAddIntent)
+                        }
+                        R.id.itemDelete -> {
+                            MemoDAO.deleteData(this@MemoInfoActivity,position)
+                            finish()
+                        }
+                    }
+                    false
+                }
+
+                // back button 세팅
+                setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material)
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                    navigationIcon?.colorFilter = BlendModeColorFilter(Color.WHITE, BlendMode.SRC_ATOP)
+                } else {
+                    navigationIcon?.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP)
+                }
+
+                setNavigationOnClickListener {
+                    finish()
+                }
+            }
+        }
     }
 }
